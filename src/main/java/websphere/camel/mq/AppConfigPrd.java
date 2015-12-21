47a1c86dc2da563d3304b55a94102eb253dbad12
Jamie Archibald <jarchibald@westjet.com>
2015-12-21 13:55:27 -0700

    More updates

--- src/main/java/websphere/camel/mq/AppConfigPrd.java
+++ src/main/java/websphere/camel/mq/AppConfigPrd.java
@@ -1,7 +1,11 @@
 package websphere.camel.mq;
 
+import javax.jms.ConnectionFactory;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jndi.JndiObjectFactoryBean;
 
 /**
  * In staging and production we connect to MQ using JNDI
@@ -11,4 +15,13 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 public class AppConfigPrd {
 
+	@Bean
+	public JndiObjectFactoryBean connectionFactory() {
+		JndiObjectFactoryBean factory = new JndiObjectFactoryBean();
+		factory.setProxyInterface(ConnectionFactory.class);
+		factory.setCache(true);
+		factory.setLookupOnStartup(true);
+		factory.setJndiName("java:comp/env/jms/InfoQueue");
+		return factory;
+	}
 }
--- src/main/java/websphere/camel/mq/LoggerService.java
+++ src/main/java/websphere/camel/mq/LoggerService.java
@@ -1,7 +1,6 @@
 package websphere.camel.mq;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 
 /**
@@ -13,7 +12,8 @@ public class LoggerService extends RouteBuilder {
 	@Override
 	public void configure() throws Exception {
 		from("jms:queue:INFO.Q")
+		.from("jms:queue:ERROR.Q")
 		.convertBodyTo(String.class)
-		.to("stream:out");
+		.log(LoggingLevel.INFO, LoggerService.class.getName(), "${body}");
 	}
 }
